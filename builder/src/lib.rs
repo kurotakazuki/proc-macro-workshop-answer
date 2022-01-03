e4f292d7e4f656efa592a0fa555b83c577d7ee4e
kurotakazuki <kabisakanakabi@gmail.com>
2022-01-03 17:10:31 +0900

    builder/02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,9 +1,30 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
-    eprintln!("{:#?}", input);
-    TokenStream::new()
+    let name = &input.ident;
+    let builder_name = format!("{}Builder", name);
+    let builder_ident = Ident::new(&builder_name, name.span());
+    quote! {
+        struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    }
+    .into()
 }
